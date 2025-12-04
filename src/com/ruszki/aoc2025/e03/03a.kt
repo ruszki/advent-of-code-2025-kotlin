51e5f506bf3c8210c1ba647376ccd5c60fdab1cc
Gergo Vladiszavlyev <github@ruszki.com>
2025-12-04 07:09:09 +0100

    03a: Add real data

--- src/com/ruszki/aoc2025/e03/03a.kt
+++ src/com/ruszki/aoc2025/e03/03a.kt
@@ -1,11 +1,6 @@
 package com.ruszki.aoc2025.e03
 
 fun main() {
-    println("max joltage of 987654321111111: ${Battery.from("987654321111111").maximumJoltage()}")
-    println("max joltage of 811111111111119: ${Battery.from("811111111111119").maximumJoltage()}")
-    println("max joltage of 234234234234278: ${Battery.from("234234234234278").maximumJoltage()}")
-    println("max joltage of 818181911112111: ${Battery.from("818181911112111").maximumJoltage()}")
-
     val result = BatteryRack.from("src/input/03.txt").outputJoltage()
 
     println("output joltage: $result")
