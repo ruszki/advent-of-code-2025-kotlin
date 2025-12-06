1d0560006e3b83a603b5e7c30548995650170e9a
Gergo Vladiszavlyev <github@ruszki.com>
2025-12-06 07:37:15 +0100

    06a: Add fake loader for homeworks

--- src/com/ruszki/aoc2025/e06/06a.kt
+++ src/com/ruszki/aoc2025/e06/06a.kt
@@ -39,31 +39,7 @@ import com.ruszki.aoc2025.e05.Inventory
  */
 
 fun main() {
-    val mathHomework = MathHomework()
-
-    val problem1 = mathHomework.addProblem()
-    problem1.problemType = ProblemType.MULTIPLICATION
-    problem1.addNumber(123uL)
-    problem1.addNumber(45uL)
-    problem1.addNumber(6uL)
-
-    val problem2 = mathHomework.addProblem()
-    problem2.problemType = ProblemType.ADDITION
-    problem2.addNumber(328uL)
-    problem2.addNumber(64uL)
-    problem2.addNumber(98uL)
-
-    val problem3 = mathHomework.addProblem()
-    problem3.problemType = ProblemType.MULTIPLICATION
-    problem3.addNumber(51uL)
-    problem3.addNumber(387uL)
-    problem3.addNumber(215uL)
-
-    val problem4 = mathHomework.addProblem()
-    problem4.problemType = ProblemType.ADDITION
-    problem4.addNumber(64uL)
-    problem4.addNumber(23uL)
-    problem4.addNumber(314uL)
+    val mathHomework = MathHomework.load()
 
     println("The result is ${mathHomework.result()}")
 }
--- src/com/ruszki/aoc2025/e06/MathHomework.kt
+++ src/com/ruszki/aoc2025/e06/MathHomework.kt
@@ -14,4 +14,36 @@ class MathHomework {
 
         return problem
     }
+
+    companion object {
+        fun load(): MathHomework {
+            val mathHomework = MathHomework()
+
+            val problem1 = mathHomework.addProblem()
+            problem1.problemType = ProblemType.MULTIPLICATION
+            problem1.addNumber(123uL)
+            problem1.addNumber(45uL)
+            problem1.addNumber(6uL)
+
+            val problem2 = mathHomework.addProblem()
+            problem2.problemType = ProblemType.ADDITION
+            problem2.addNumber(328uL)
+            problem2.addNumber(64uL)
+            problem2.addNumber(98uL)
+
+            val problem3 = mathHomework.addProblem()
+            problem3.problemType = ProblemType.MULTIPLICATION
+            problem3.addNumber(51uL)
+            problem3.addNumber(387uL)
+            problem3.addNumber(215uL)
+
+            val problem4 = mathHomework.addProblem()
+            problem4.problemType = ProblemType.ADDITION
+            problem4.addNumber(64uL)
+            problem4.addNumber(23uL)
+            problem4.addNumber(314uL)
+
+            return mathHomework
+        }
+    }
 }
